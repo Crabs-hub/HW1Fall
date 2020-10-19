1be16a76536a5faf546f4942ea9d97b6c5003315
Crabs-hub <72178189+Crabs-hub@users.noreply.github.com>
2020-10-18 18:30:21 -0700

    Answers

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -7,12 +7,12 @@ public class Problem2Test {
     // Example test
     // Okay to change
     @Test
-    public boolean testBubbleSort() {
+    public void testBubbleSort() {
         // add your tests here
         int sort[] = {6, 4, 5, 3, 2, 1};
         int answer[] = {1, 2, 3, 4, 5, 6};
         Problem2 test = new Problem2();
         test.bubbleSort(sort);
-        return true;
+        assertArrayEquals(sort, answer);
     }
 }
