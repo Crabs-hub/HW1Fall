239ad1bb34f65bd41a30ae7dab5e5c865d2dba44
Crabs-hub <72178189+Crabs-hub@users.noreply.github.com>
2020-10-18 18:15:27 -0700

    Test.

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -1,27 +1,18 @@
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class Test2{
+public class Test2 {
 
     // Example test
     // Okay to change
     @Test
     public boolean testBubbleSort() {
         // add your tests here
-
-        boolean check = true;
-        for (int i = 0 ; i < data.length ; i++){
-            if(data[i] < data[i+1]){
-
-            }
-            else if(data[i] > data[i+1]){
-                check = false;
-            }
-        }
-        return check;
+        int sort[] = {6, 4, 5, 3, 2, 1};
+        int answer[] = {1, 2, 3, 4, 5, 6};
+        Problem2 test = new Problem2();
+        test.bubbleSort(sort);
+        return true;
     }
-
-
 }
